e27d5ef83a75cb6f10da0b72f974d4387f288012
DESKTOP-3EB1GQJ\LSM <seung33344@gmail.com>
2022-04-09 16:38:51 +0900

    user / resource.user / 0409/1638

--- puddingPage/src/main/webapp/WEB-INF/resource/user/js/common.js
+++ puddingPage/src/main/webapp/WEB-INF/resource/user/js/common.js
@@ -1,10 +1,34 @@
-//나중에 jsp 교체
+
+// 마이페이지 스크롤
+var realclick = $("#banner"); 
+var realclick_top = 155;  //애니매이션 이동 위치
+
+
 
 $(document).ready(function(){
+    //나중에 jsp 교체
     $("#headers").load("../views/ur_header.html");
 
     $("#footers").load("../views/ur_footer.html");
     
     $("#submenu").load("../views/um_submenu.html");
-    faqclick(); //faq 질문 누르기
+
+    realclick.animate( { "top": $(document).scrollTop() + realclick_top + "px" }, 0 );  
+
+    // 마이페이지 베너 스크롤
+    $(window).scroll(function(){
+        
+        realclick.stop();
+
+        realclick.animate( { "top": $(document).scrollTop() + realclick_top + "px" }, 1000 );
+
+       
+    });
+
+    //faq 질문 누르기
+    FaqBox__init();
+
 })
+
+
+
